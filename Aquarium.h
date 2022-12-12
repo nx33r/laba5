c20312d57e54dd2e29305f75de1e4c482cf33cb3
nx33r <119191723+nx33r@users.noreply.github.com>
2022-12-12 19:29:12 +0100

    Update Aquarium.h

--- Aquarium.h
+++ Aquarium.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <vector>
+#include "Fish.h"
+
+class Aquarium
+{
+	vector<Fish> fishes;
+	int countOfFish;
+	double totalSpace;
+	double freeSpace;
+
+public:
+	Aquarium(int space);
+
+	void addFish(Fish fish);
+	void printBiggest();
+	void print();
+};
 
